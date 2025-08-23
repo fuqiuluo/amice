dc867457f19724e4f32ee708667e10bc2b450cba
Mona Lisa <mut@vip.qq.com>
2025-08-23 14:51:56 +0800

    feat: add random pointer chain construction in `AliasAccess`
    - Implemented functionality to build random pointer chains for enhanced data obfuscation.
    - Added `shuffle_raw_box` and `loose_raw_box` options to customize RawBox handling.
    - Updated `AliasAccess` to process functions with pointer chain transformations.
    - Enhanced error logging for alias access processing failures.

--- src/aotu/alias_access/mod.rs
+++ src/aotu/alias_access/mod.rs
@@ -6,17 +6,24 @@ use amice_llvm::module_utils::{VerifyResult, verify_function};
 use amice_macro::amice;
 use llvm_plugin::inkwell::module::Module;
 use llvm_plugin::{LlvmModulePass, ModuleAnalysisManager, PreservedAnalyses};
-use log::warn;
+use log::{error, warn};
 
 #[amice(priority = 1112, name = "AliasAccess", position = PassPosition::PipelineStart)]
 #[derive(Default)]
 pub struct AliasAccess {
     enable: bool,
+    /// 打乱RawBox顺序
+    shuffle_raw_box: bool,
+    /// 宽松的RawBox，填充垃圾
+    loose_raw_box: bool,
 }
 
 impl AmicePassLoadable for AliasAccess {
     fn init(&mut self, cfg: &Config, position: PassPosition) -> bool {
         self.enable = cfg.alias_access.enable;
+
+        self.shuffle_raw_box = false; // todo: 暂时先关着
+        self.loose_raw_box = false;
 
         self.enable
     }
@@ -29,6 +36,11 @@ impl LlvmModulePass for AliasAccess {
         }
 
         for function in module.get_functions() {
+            if let Err(e) = pointer_chain::do_alias_access(self, module, function) {
+                error!("(alias-access) failed to process function {:?}: {}", function.get_name(), e);
+                continue;
+            }
+
             if let VerifyResult::Broken(e) = verify_function(function) {
                 warn!("(alias-access) function {:?} verify failed: {}", function.get_name(), e);
             }
