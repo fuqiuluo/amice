c436c65adfb77d68496e7f9fd86223b1632cafaf
Mona Lisa <mut@vip.qq.com>
2025-08-26 03:27:55 +0800

    fix: update LLVM version check to exclude unsupported versions (17-20)

--- src/aotu/mod.rs
+++ src/aotu/mod.rs
@@ -13,7 +13,12 @@ pub mod split_basic_block;
 pub mod string_encryption;
 pub mod vm_flatten;
 
-#[cfg(any(feature = "llvm15-0", feature = "llvm16-0",))]
+#[cfg(not(any(
+    feature = "llvm17-0",
+    feature = "llvm18-1",
+    feature = "llvm19-1",
+    feature = "llvm20-1"
+)))]
 #[macro_export]
 macro_rules! ptr_type {
     ($cx:ident, $ty:ident) => {
