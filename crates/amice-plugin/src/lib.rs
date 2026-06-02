d61411b604b30e47f81b7d16e3c23a051cb6ff5b
fuqiuluo <mut@vip.qq.com>
2026-06-02 22:21:37 +0800

    chore: remove stale amice-plugin docs

--- crates/amice-plugin/src/lib.rs
+++ crates/amice-plugin/src/lib.rs
@@ -1,100 +1,4 @@
-//! [![github]](https://github.com/jamesmth/llvm-plugin-rs)&ensp;[![crates-io]](https://crates.io/crates/llvm-plugin)
-//!
-//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
-//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
-//!
-//! <br>
-//!
-//! This crate gives the ability to safely implement passes for the [new LLVM pass manager],
-//! by leveraging the strongly typed interface  provided by [Inkwell].
-//!
-//! If you have never developed LLVM passes before, you can take a look at the available
-//! [examples]. They will (hopefully) give you a better idea of how to use this crate.
-//!
-//! If you want a deeper understanding of the many concepts surrounding the new LLVM
-//! pass manager, you should read the [official LLVM documentation].
-//!
-//! [Inkwell]: https://github.com/TheDan64/inkwell
-//! [new LLVM pass manager]: https://blog.llvm.org/posts/2021-03-26-the-new-pass-manager/
-//! [examples]: https://github.com/jamesmth/llvm-plugin-rs/tree/master/examples
-//! [official LLVM documentation]: https://llvm.org/docs/NewPassManager.html
-//!
-//! # Getting started
-//!
-//! An LLVM plugin is merely a dylib that is given a [PassBuilder] by the LLVM tool
-//! (e.g. [opt], [lld]) loading it. A [PassBuilder] allows registering callbacks on
-//! specific actions being performed by the LLVM tool.
-//!
-//! For instance, the `--passes` parameter of [opt] allows specifying a custom pass pipeline
-//! to be run on a given IR module. A plugin could therefore register a callback for
-//! parsing an element of the given pipeline (e.g. a pass name), in order to insert a custom
-//! pass to run by [opt].
-//!
-//! The following code illustrates the idea:
-//!
-//! ```no_run
-//! # use llvm_plugin::inkwell::module::Module;
-//! # use llvm_plugin::{
-//! #     LlvmModulePass, ModuleAnalysisManager, PassBuilder, PipelineParsing, PreservedAnalyses,
-//! # };
-//! // A name and version is required.
-//! #[llvm_plugin::plugin(name = "plugin_name", version = "0.1")]
-//! fn plugin_registrar(builder: &mut PassBuilder) {
-//!     // Add a callback to parse a name from the textual representation of
-//!     // the pipeline to be run.
-//!     builder.add_module_pipeline_parsing_callback(|name, manager| {
-//!         if name == "custom-pass" {
-//!             // the input pipeline contains the name "custom-pass",
-//!             // so we add our custom pass to the pass manager
-//!             manager.add_pass(CustomPass);
-//!
-//!             // we notify the caller that we were able to parse
-//!             // the given name
-//!             PipelineParsing::Parsed
-//!         } else {
-//!             // in any other cases, we notify the caller that our
-//!             // callback wasn't able to parse the given name
-//!             PipelineParsing::NotParsed
-//!         }
-//!     });
-//! }
-//!
-//! struct CustomPass;
-//! impl LlvmModulePass for CustomPass {
-//!     fn run_pass(
-//!         &self,
-//!         module: &mut Module,
-//!         manager: &ModuleAnalysisManager
-//!     ) -> PreservedAnalyses {
-//!         // transform the IR
-//!         # PreservedAnalyses::All
-//!     }
-//! }
-//! ```
-//!
-//! Now, executing this command would run our custom pass on the input module:
-//!
-//! ```bash
-//! opt --load-pass-plugin=libplugin.so --passes=custom-pass module.bc -disable-output
-//! ```
-//!
-//! However, executing this command would not (`custom-pass2` cannot be parsed by our plugin):
-//!
-//! ```bash
-//! opt --load-pass-plugin=libplugin.so --passes=custom-pass2 module.bc -disable-output
-//! ```
-//!
-//! More callbacks are available, read the [PassBuilder] documentation for more details.
-//!
-//! # A note on Windows
-//!
-//! On this platform, LLVM plugins need the LLVM symbols directly from the executable loading
-//! them (most of the time `opt.exe` or `lld.exe`). Therefore, you need to specify the
-//! additional feature `win-link-opt` or `win-link-lld` while building a plugin. The former
-//! will link the plugin to `opt.lib`, the latter being for `lld.lib`.
-//!
-//! [opt]: https://www.llvm.org/docs/CommandGuide/opt.html
-//! [lld]: https://lld.llvm.org/
+//! In-tree LLVM pass-plugin runtime used by amice.
 
 #![deny(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
@@ -199,27 +103,6 @@ pub trait LlvmModuleAnalysis {
     /// ones used by the LLVM tool that loads your plugin, you should use static
     /// variables' addresses as well.
     ///
-    /// # Example
-    ///
-    /// ```
-    /// # use llvm_plugin::inkwell::module::Module;
-    /// # use llvm_plugin::{AnalysisKey, LlvmModuleAnalysis, ModuleAnalysisManager};
-    /// # struct Analysis;
-    /// # impl LlvmModuleAnalysis for Analysis {
-    /// #    type Result = ();
-    /// #    fn run_analysis(
-    /// #        &self,
-    /// #        _module: &Module,
-    /// #        _manager: &ModuleAnalysisManager,
-    /// #    ) -> Self::Result {}
-    /// #
-    /// fn id() -> AnalysisKey {
-    ///     static ID: u8 = 0;
-    ///     &ID
-    /// }
-    /// # }
-    /// ```
-    ///
     /// [opt]: https://www.llvm.org/docs/CommandGuide/opt.html
     /// [lld]: https://lld.llvm.org/
     fn id() -> AnalysisKey;
@@ -257,27 +140,6 @@ pub trait LlvmFunctionAnalysis {
     /// To make sure your custom analysis types don't collide with the builtin
     /// ones used by the LLVM tool that loads your plugin, you should use static
     /// variables' addresses as well.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use llvm_plugin::inkwell::values::FunctionValue;
-    /// # use llvm_plugin::{AnalysisKey, LlvmFunctionAnalysis, FunctionAnalysisManager};
-    /// # struct Analysis;
-    /// # impl LlvmFunctionAnalysis for Analysis {
-    /// #    type Result = ();
-    /// #    fn run_analysis(
-    /// #        &self,
-    /// #        _function: &FunctionValue,
-    /// #        _manager: &FunctionAnalysisManager,
-    /// #    ) -> Self::Result {}
-    /// #
-    /// fn id() -> AnalysisKey {
-    ///     static ID: u8 = 0;
-    ///     &ID
-    /// }
-    /// # }
-    /// ```
     ///
     /// [opt]: https://www.llvm.org/docs/CommandGuide/opt.html
     /// [lld]: https://lld.llvm.org/
